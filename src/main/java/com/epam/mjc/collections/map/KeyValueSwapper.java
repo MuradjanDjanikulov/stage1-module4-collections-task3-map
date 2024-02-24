6b0f257a31f61c37f6f3fb324cddff4729798e97
murod <murad.djanikulov@gmail.com>
2024-02-24 19:34:38 +0500

    map task3 done

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,12 @@
 package com.epam.mjc.collections.map;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> result = new TreeMap<>();
+        sourceMap.forEach((key, value) -> result.put(value, key));
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,19 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> result = new HashMap<>();
+        sentence = sentence.toLowerCase();
+        String[] splitted = sentence.split("\\s+|\\p{Punct}");
+        for (String s : splitted) {
+            if(!Pattern.matches("(^$|^\"$|^_$|^\\.$)", s)) {
+                result.put(s, result.getOrDefault(s, 0) + 1);
+            }
+        }
+        return result;
     }
 }
